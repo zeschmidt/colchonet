857ecf7253ecac936e68712007ba1913a85ac221
zeschmidt <ze.schmidt@gmail.com>
2013-12-12 22:21:13 -0200

    modificado action new adicionado actions index e show

--- colchonet/app/controllers/users_controller.rb
+++ colchonet/app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+
 	def new
+		@user = User.new
 	end
+
+	def create 
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to @user,
+				notice: 'Cadastro criado com sucesso'
+		else
+			render action: :new
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:email, :full_name, :location, :password,:bio)
+	end
+
 end
